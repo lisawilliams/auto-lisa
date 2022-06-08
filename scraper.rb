5ae0131ea10e668a812974ccd114c5bd87684b72
lisawilliams <lisatmh@gmail.com>
2022-06-07 21:49:02 -0400

    beginning to extract data from parsed output

--- scraper.rb
+++ scraper.rb
@@ -7,10 +7,25 @@ require 'byebug'
 # build methods
 
 def scraper
-	url = "https://www.wgbh.org/news/"
+	url = "https://careers.journalists.org/jobs/"
 	unparsed_page = HTTParty.get(url)
 	parsed_page = Nokogiri::HTML(unparsed_page.body)
-	byebug
+
+	# variable to store all the job listings on the page we grabbed above
+	job_listings = parsed_page.css('div.card-title') #25 jobs
+
+	#now iterate through the individual listings
+	job_listings.each do |job_listing|
+
+		job = {
+			title: job_listing.css("div.card-title").text,
+			company: job_listing.css("div.card-subtitle").text,
+			location: job_listing.css("div.card-text").text,
+		}
+		
+	 	byebug
+	end	
+
 end
 
 scraper
