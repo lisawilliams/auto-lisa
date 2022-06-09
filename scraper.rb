0d0a4b8a634fe96defa89b256dd935d75fda42d9
lisawilliams <lisatmh@gmail.com>
2022-06-09 10:45:51 -0400

    experimenting with different ways of parsing HTML and CSS using nokogiri

--- scraper.rb
+++ scraper.rb
@@ -13,19 +13,25 @@ def scraper
 
 	# variable to store all the job listings on the page we grabbed above
 	job_listings = parsed_page.css('div.bti-job-search-results') #25 jobs
+	#array to store individual jobs
+	jobs = Array.new
 
 	#now iterate through the individual listings
 	job_listings.each do |job_listing|
 
 		job = {
-			title: job_listing.css("div.card-title").text,
-			company: job_listing.css("div.card-subtitle").text,
-			location: job_listing.css("p.card-text").text,
-			url: job_listing.css("div.data-job-path")
+			title: job_listing.css('div.card-title').text,
+			company: job_listing.css('div.card-subtitle').text,
+			location: job_listing.css('p.card-text').text,
+			url: job_listing.css('id[data-job-path]')
 		}
 		
-	 	byebug
+		jobs << job
+
+	 
 	end	
+
+	byebug
 
 end
 
